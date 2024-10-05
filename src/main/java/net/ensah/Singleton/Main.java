1fe40ece430729f212db4a3e51b68e753644109e
abdelilah <abdelilah.saouir@etu.uae.ac.ma>
2024-10-05 23:00:02 +0100

    update Singleton pattern

--- src/main/java/net/ensah/Singleton/Main.java
+++ src/main/java/net/ensah/Singleton/Main.java
@@ -3,7 +3,8 @@ package net.ensah.Singleton;
 public class Main {
 
     public static void main(String[] args) {
-         Person person= Person.getInstance();
+         Person p1= Person.getInstance();
+         Person p2= Person.getInstance();
 
     }
 }
